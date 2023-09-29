8abb3e8f46e391ca13b19e2134404a28d13917f7
itsubaki <1759459+itsubaki@users.noreply.github.com>
2023-09-30 00:46:27 +0900

    Add color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,4 +18,45 @@ export class AppComponent implements OnInit {
       this.repos = repos;
     });
   }
+
+  color(name: string): string {
+    switch (name) {
+      case "C":
+        return "c_color";
+      case "Go":
+        return "go_color";
+      case "Java":
+        return "java_color";
+      case "Scala":
+        return "scala_color";
+      case "Elixir":
+        return "elixir_color";
+      case "JavaScript":
+        return "javascript_color";
+      case "TypeScript":
+        return "typescript_color";
+      case "HCL":
+        return "hcl_color";
+      case "Gherkin":
+        return "gherkin_color";
+      case "Shell":
+        return "shell_color";
+      case "Makefile":
+        return "makefile_color";
+      case "CMake":
+        return "cmake_color";
+      case "HTML":
+        return "html_color";
+      case "CSS":
+        return "css_color";
+      case "Python":
+        return "python_color";
+      case "Rust":
+        return "rust_color";
+      case "Other":
+        return "other_color";
+      default:
+        return "other_color";
+    }
+  }
 }
